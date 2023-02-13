bc51ad17fc84830a90f369a906b41f75c9552b90
Angel Jesse Dampil <108660194+Mochimochi07@users.noreply.github.com>
2023-02-13 16:44:28 +0800

    Update velocity.cpp

--- velocity.cpp
+++ velocity.cpp
@@ -11,22 +11,22 @@ int main() {
   double currentVelocity = initialVelocity;
   double currentPosition = initialPosition;
 
-  // simulate for 10 seconds
+  
   while (currentTime <= 10.0) {
-    // calculate the velocity at the current time
+    
     currentVelocity = initialVelocity + acceleration * currentTime;
 
-    // calculate the position at the current time
+
     currentPosition = initialPosition + initialVelocity * currentTime + 
       0.5 * acceleration * currentTime * currentTime;
 
-    // output the results
+   
     std::cout << "Time: " << currentTime << " s" << std::endl;
     std::cout << "Velocity: " << currentVelocity << " m/s" << std::endl;
     std::cout << "Position: " << currentPosition << " m" << std::endl;
     std::cout << std::endl;
 
-    // update the time
+    
     currentTime += timeStep;
   }
 
